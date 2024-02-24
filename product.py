2ccd289d8d784ede49d409fc2ae283730ce2135b
Your Name <you@example.com>
2024-02-24 03:40:33 +0300

    Added magic methods __str__ and __add__

--- product.py
+++ product.py
@@ -5,17 +5,13 @@ class Product:
         self._quantity = quantity
 
     @property
-    def name(self):
-        return self._name
-
-    @property
     def price(self):
         return self._price
 
     @price.setter
     def price(self, value):
         if value < 0:
-            raise ValueError("Цена должна быть неотрицательной")
+            raise ValueError("Price must be non-negative")
         self._price = value
 
     @property
@@ -25,9 +21,15 @@ class Product:
     @quantity.setter
     def quantity(self, value):
         if value < 0:
-            raise ValueError("Количество должно быть неотрицательным")
+            raise ValueError("Quantity must be non-negative")
         self._quantity = value
 
-    @classmethod
-    def create_product(cls, name, price, quantity):
-        return cls(name, price, quantity)
+    def __str__(self):
+        return f"{self._name}, {self._price} руб. Остаток: {self._quantity} шт."
+
+    def __add__(self, other):
+        total_price_self = self._price * self._quantity
+        total_price_other = other._price * other._quantity
+        total_quantity = self._quantity + other._quantity
+        new_price = (total_price_self + total_price_other) / total_quantity
+        return Product("Merged Product", new_price, total_quantity)
