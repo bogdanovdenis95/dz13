8cd1af8bba6d63a8556d01c65c9f43e1a2276321
Your Name <you@example.com>
2024-03-06 06:17:35 +0300

    Initial commit

--- product.py
+++ product.py
@@ -1,33 +1,33 @@
 from abc import ABC, abstractmethod
 
 class AbstractProduct(ABC):
-    @abstractmethod
-    def calculate_total_price(self):
-        pass
-
-    @abstractmethod
-    def __str__(self):
-        pass
-
-class LoggingMixin:
-    def __repr__(self):
-        attributes = ', '.join(f"{key}={value}" for key, value in self.__dict__.items())
-        return f"{self.__class__.__name__}({attributes})"
-
-class Product(AbstractProduct, LoggingMixin):
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
 
+    @abstractmethod
+    def create_product(self):
+        pass
+
+    @abstractmethod
     def __str__(self):
-        return f"{self.name}, цена: {self.price}, описание: {self.description}"
+        pass
 
+    @abstractmethod
     def calculate_total_price(self):
-        return self.price * self.quantity
+        pass
 
-class Smartphone(Product):
+class LoggingMixin:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        attributes = ', '.join(f"{key}={value}" for key, value in self.__dict__.items())
+        return f"{self.__class__.__name__}({attributes})"
+
+class Smartphone(AbstractProduct, LoggingMixin):
     def __init__(self, name, description, price, quantity, performance, model, memory, color):
         super().__init__(name, description, price, quantity)
         self.performance = performance
@@ -35,9 +35,29 @@ class Smartphone(Product):
         self.memory = memory
         self.color = color
 
-class LawnGrass(Product):
+    def create_product(self):
+        # Логика создания продукта
+        pass
+
+    def __str__(self):
+        return f"{self.name}, цена: {self.price}, описание: {self.description}"
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+class LawnGrass(AbstractProduct, LoggingMixin):
     def __init__(self, name, description, price, quantity, country_of_origin, germination_period, color):
         super().__init__(name, description, price, quantity)
         self.country_of_origin = country_of_origin
         self.germination_period = germination_period
         self.color = color
+
+    def create_product(self):
+        # Логика создания продукта
+        pass
+
+    def __str__(self):
+        return f"{self.name}, цена: {self.price}, описание: {self.description}"
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
